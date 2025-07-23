efe219396936811b8b505cd74c134a9185575772
Disha Maheshwari <disha.maheshwari@icloud.com>
2025-07-23 14:44:37 -0400

    changed text content & position

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Look out!")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
